79b0a2b2e091ce5abe8e6c27954dc50bc5110ffe
Collin Hasken <hasken.collin@gmail.com>
2018-12-04 19:49:16 -0600

    Fix up

--- Day_4/src.py
+++ Day_4/src.py
@@ -1,20 +1,18 @@
-import os
+import operator
 
 
 def part_1():
-    with open("C:/Users/Collin/Documents/GitHub/Advent-2018/Day_4/part1.txt", 'r') as file:
+    with open("part1.txt", 'r') as file:
         guards_shifts = {}
         for line in file:
-            time = line.split("]")[0]
-            info = line.split("]")[1]
-            month = int(time.split("-")[1])
-            date_time = time.split("-")[2]
-            date = int(date_time.split(" ")[0])
-            hour = int(date_time.split(" ")[1].split(":")[0])
-            adjusted = False
+            time, info = line.split("]")
+            month, date_time = [date_info for date_info in time.split("-")[1:]]
+            month = int(month)
+            date, time = date_time.split(" ")
+            date = int(date)
+            hour, minutes = [int(d) for d in time.split(":")]
             if hour != 0:
-                adjusted = True
-                time = 0
+                minutes = 0
                 date += 1
                 month_lim = 31
                 if month == 4 or month == 6 or month == 9 or month == 11:
@@ -26,8 +24,6 @@ def part_1():
                     month += 1
                 if month == 13:
                     month = 1
-            else:
-                time = int(date_time.split(" ")[1].split(":")[1])
             id = ''.join(x for x in info if x.isdigit())
             if id != '':
                 id = int(id)
@@ -36,30 +32,30 @@ def part_1():
                 guards_shifts[month] = {}
             if date not in guards_shifts[month]:
                 guards_shifts[month][date] = [id, [0 for x in range(60)]]
-            if id is not '':
+            if id != '':
                 guards_shifts[month][date][0] = id
             if not asleep:
-                if guards_shifts[month][date][1][time] == 3:
+                if guards_shifts[month][date][1][minutes] == 3:
                     continue
-                if guards_shifts[month][date][1][time] == 0:
-                    if adjusted:
-                        guards_shifts[month][date][1][time] = 0
+                if guards_shifts[month][date][1][minutes] == 0:
+                    if hour != 0:
+                        guards_shifts[month][date][1][minutes] = 0
                     else:
-                        guards_shifts[month][date][1][time] = 2
+                        guards_shifts[month][date][1][minutes] = 2
                     continue
 
-                if adjusted:
-                    guards_shifts[month][date][1][time] = 0
+                if hour != 0:
+                    guards_shifts[month][date][1][minutes] = 0
                 else:
-                    guards_shifts[month][date][1][time] = 2
+                    guards_shifts[month][date][1][minutes] = 2
 
-                for min in range(time + 1, 60):
+                for min in range(minutes + 1, 60):
                     if guards_shifts[month][date][1][min] == 3 or guards_shifts[month][date][1][min] == 2:
                         break
                     guards_shifts[month][date][1][min] = 0
             else:
-                guards_shifts[month][date][1][time] = 3
-                for min in range(time + 1, 60):
+                guards_shifts[month][date][1][minutes] = 3
+                for min in range(minutes + 1, 60):
                     if guards_shifts[month][date][1][min] == 2 or guards_shifts[month][date][1][min] == 3:
                         break
                     guards_shifts[month][date][1][min] = 1
@@ -68,46 +64,33 @@ def part_1():
             for date, shift_data in date_data.items():
                 if shift_data[0] not in sleep_count:
                     sleep_count[shift_data[0]] = 0
-                sleep_count[shift_data[0]] += len([1 for x in shift_data[1] if x == 1 or x == 3])
-        print(sleep_count)
-        most_id = 0
-        most_sleep = 0
-        for id, sleep in sleep_count.items():
-            if sleep > most_sleep:
-                most_sleep = sleep
-                most_id = id
-        print(most_id)
+                sleep_count[shift_data[0]] += shift_data[1].count(1) + shift_data[1].count(3)
+        sorted_sleep_count = sorted(sleep_count.items(), key=operator.itemgetter(1), reverse=True)
 
         minutes = [0 for x in range(60)]
         for month, date_data in guards_shifts.items():
             for date, shift_data in date_data.items():
-                if shift_data[0] == most_id:
+                if shift_data[0] == sorted_sleep_count[0][0]:
                     for minute in range(60):
                         if shift_data[1][minute] == 1 or shift_data[1][minute] == 3:
                             minutes[minute] += 1
-        print(minutes)
-        maxs = 0
-        for mins in range(60):
-            if minutes[mins] > minutes[maxs]:
-                maxs = mins
-        print(maxs)
-        print(most_id * maxs)
+
+        max_minute = max(enumerate(minutes), key=lambda m: m[1])[0]
+        print(sorted_sleep_count[0][0] * max_minute)
 
 
 def part_2():
     with open("C:/Users/Collin/Documents/GitHub/Advent-2018/Day_4/part1.txt", 'r') as file:
         guards_shifts = {}
         for line in file:
-            time = line.split("]")[0]
-            info = line.split("]")[1]
-            month = int(time.split("-")[1])
-            date_time = time.split("-")[2]
-            date = int(date_time.split(" ")[0])
-            hour = int(date_time.split(" ")[1].split(":")[0])
-            adjusted = False
+            time, info = line.split("]")
+            month, date_time = [date_info for date_info in time.split("-")[1:]]
+            month = int(month)
+            date, time = date_time.split(" ")
+            date = int(date)
+            hour, minutes = [int(d) for d in time.split(":")]
             if hour != 0:
-                adjusted = True
-                time = 0
+                minutes = 0
                 date += 1
                 month_lim = 31
                 if month == 4 or month == 6 or month == 9 or month == 11:
@@ -119,8 +102,6 @@ def part_2():
                     month += 1
                 if month == 13:
                     month = 1
-            else:
-                time = int(date_time.split(" ")[1].split(":")[1])
             id = ''.join(x for x in info if x.isdigit())
             if id != '':
                 id = int(id)
@@ -129,30 +110,30 @@ def part_2():
                 guards_shifts[month] = {}
             if date not in guards_shifts[month]:
                 guards_shifts[month][date] = [id, [0 for x in range(60)]]
-            if id is not '':
+            if id != '':
                 guards_shifts[month][date][0] = id
             if not asleep:
-                if guards_shifts[month][date][1][time] == 3:
+                if guards_shifts[month][date][1][minutes] == 3:
                     continue
-                if guards_shifts[month][date][1][time] == 0:
-                    if adjusted:
-                        guards_shifts[month][date][1][time] = 0
+                if guards_shifts[month][date][1][minutes] == 0:
+                    if hour != 0:
+                        guards_shifts[month][date][1][minutes] = 0
                     else:
-                        guards_shifts[month][date][1][time] = 2
+                        guards_shifts[month][date][1][minutes] = 2
                     continue
 
-                if adjusted:
-                    guards_shifts[month][date][1][time] = 0
+                if hour != 0:
+                    guards_shifts[month][date][1][minutes] = 0
                 else:
-                    guards_shifts[month][date][1][time] = 2
+                    guards_shifts[month][date][1][minutes] = 2
 
-                for min in range(time + 1, 60):
+                for min in range(minutes + 1, 60):
                     if guards_shifts[month][date][1][min] == 3 or guards_shifts[month][date][1][min] == 2:
                         break
                     guards_shifts[month][date][1][min] = 0
             else:
-                guards_shifts[month][date][1][time] = 3
-                for min in range(time + 1, 60):
+                guards_shifts[month][date][1][minutes] = 3
+                for min in range(minutes + 1, 60):
                     if guards_shifts[month][date][1][min] == 2 or guards_shifts[month][date][1][min] == 3:
                         break
                     guards_shifts[month][date][1][min] = 1
@@ -161,24 +142,14 @@ def part_2():
         for month, date_data in guards_shifts.items():
             for date, shift_data in date_data.items():
                 if shift_data[0] not in minutes:
-                    minutes[shift_data[0]] = [0 for x in range(60)]
+                    minutes[shift_data[0]] = [[x, 0] for x in range(60)]
                 for minute in range(60):
                     if shift_data[1][minute] == 1 or shift_data[1][minute] == 3:
-                        minutes[shift_data[0]][minute] += 1
-        print(minutes)
-        maxs = 0
-        max_id = 0
-        for id, mins in minutes.items():
-            local_max = 0
-            for min in range(60):
-                if mins[min] > mins[local_max]:
-                    local_max = min
-            if max_id not in minutes or mins[local_max] > minutes[max_id][maxs]:
-                max_id = id
-                maxs = local_max
-        print(maxs)
-        print(max_id)
-        print(max_id * maxs)
+                        minutes[shift_data[0]][minute][1] += 1
+
+        max_id, max_minutes = max(minutes.items(), key=lambda mins: max(mins[1], key=lambda m: m[1])[1])
+        max_minute = max(max_minutes, key=lambda m: m[1])[0]
+        print(max_id * max_minute)
 
 
 if __name__ == '__main__':
